1a25b1dffac6095a314f892ed1c5aa0bd90211a7
Ryan Duffield <duffield@gmail.com>
2011-10-06 11:23:49 +0100

    Return all data rather than a subset

--- stats.py
+++ stats.py
@@ -4,7 +4,8 @@ class ProcNetNetstat:
         self.key = key
 
     def _floatify(self, key):
-        self.dat.get(self.key)[key] = float(self.dat.get(self.key).get(key))
+        for sub_key in self.dat.get(key):
+            self.dat.get(key)[sub_key] = float(self.dat.get(key).get(sub_key))
 
     def run(self):
         dat = {}
@@ -20,5 +21,5 @@ class ProcNetNetstat:
                 dat[name] = dict(zip(dat[name], line))
         f.close()
         self.dat = dat
-        map(self._floatify, dat.get(self.key).keys())
-        return dat.get(self.key)
+        map(self._floatify, dat.keys())
+        return dat
